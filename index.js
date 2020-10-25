cb08f182bba2debd917e654c9293f70032a44390
Sam Henderson <jipo_00@hotmail.com>
2020-10-25 19:55:01 +0000

    initial

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function solution(A, X) {
+    var N = A.length;
+    if (N === 0) {
+        return -1;
+    }
+    var l = 0;
+    var r = N - 1;
+    while (l < r) {
+        var m = Math.ceil((l + r) / 2);
+        if (A[m] > X) {
+            r = m - 1;
+        } else {
+            l = m;
+        }
+    }
+    if (A[l] == X) {
+        return l;
+    }
+    return -1;
+}
+
+console.log(solution([1, 5, 6, 8, 15], 6))
